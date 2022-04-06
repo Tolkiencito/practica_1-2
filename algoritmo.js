8315149bf01d8b47f4b18491c11cd644e18a0233
Federico Costa <federicojoaquincosta@gmail.com>
2022-04-05 22:09:41 -0300

    ejemplos de iframe

---
+++ algoritmo.js
@@ -0,0 +1,9 @@
+console.log("funciona");
+
+document.write("<iframe width='560' height='315' src='https://www.youtube.com/embed/hsw8e1n1w4A' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
+
+document.write("<iframe src='https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d11702.316079430448!2d-58.39186460806937!3d-34.60792076170192!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sar!4v1648817436141!5m2!1ses!2sar' width='600' height='450' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>");
+
+document.write("<iframe src='https://cdn.pixabay.com/audio/2021/12/16/audio_232a4bdedf.mp3' frameborder='0'></iframe>");
+
+document.write("<iframe src='https://www.mercadolibre.com.ar/' frameborder='0'></iframe>")
